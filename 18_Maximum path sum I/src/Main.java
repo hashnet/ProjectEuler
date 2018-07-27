1275ad980060cb7bfd077f1c21ee950981fddf76
hashnet <maidul.hasan@gmail.com>
2018-07-28 02:13:10 +0400

    18_Maximum path sum I

---
+++ 18_Maximum path sum I/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner sc = new Scanner(new FileInputStream("files/input.txt"));
+//		Scanner sc = new Scanner(new FileInputStream("files/sample.txt"));
+		
+		int max = 15;
+//		int max = 4;
+		long sum[][] = new long[max][max];
+		
+		for(int i=0; i<max; i++) {
+			for(int j=0; j<=i; j++) {
+				sum[i][j] = sc.nextLong();
+			}
+		}
+			
+		for(int i=max-2; i>=0; i--) {
+			for(int j=0; j<=i; j++) {
+				sum[i][j] = Math.max(sum[i+1][j], sum[i+1][j+1]) + sum[i][j];
+			}
+		}
+		
+		System.out.println(sum[0][0]);
+	}
+}
